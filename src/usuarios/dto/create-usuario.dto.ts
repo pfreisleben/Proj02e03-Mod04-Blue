1937fd5cdf4a94d4fa243d40b98432162f602a0c
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-12-13 15:53:57 -0300

    ok

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -1,3 +1,19 @@
-import { IsInt } from 'class-validator';
+import { IsInt, IsString, IsNotEmpty, IsOptional, IsDateString } from 'class-validator';
 
-export class CreateUsuarioDto {}
+export class CreateUsuarioDto {
+  @IsNotEmpty()
+  @IsString()
+  nome: string;
+
+  @IsOptional()
+  @IsString()
+  imagem: string;
+
+  @IsString()
+  @IsNotEmpty()
+  bio: string;
+
+  @IsDateString()
+  @IsNotEmpty()
+  nascimento: Date;
+}
--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [UsuariosController],
   providers: [UsuariosService],
+  imports: [PrismaService],
 })
 export class UsuariosModule {}
