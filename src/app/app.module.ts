afd88a4d909f8f0572dcf33e76a5b685deb6a94b
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-12-13 15:38:04 -0300

    ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UsuariosModule } from 'src/usuarios/usuarios.module';
 
 @Module({
-  imports: [],
+  imports: [UsuariosModule, PrismaModule],
   controllers: [],
   providers: [],
 })
