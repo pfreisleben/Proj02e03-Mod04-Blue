8c45e901e301a696bf1de43b056caca6da7496c9
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-12-27 15:04:57 -0300

    ok

--- src/usuarios/dto/create-usuario.dto.ts
+++ src/usuarios/dto/create-usuario.dto.ts
@@ -3,12 +3,21 @@ import {
   IsNotEmpty,
   IsOptional,
   IsDateString,
+  IsEmail,
 } from 'class-validator';
 
 export class CreateUsuarioDto {
   @IsNotEmpty()
   @IsString()
   nome: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  senha: string;
 
   @IsOptional()
   @IsString()
