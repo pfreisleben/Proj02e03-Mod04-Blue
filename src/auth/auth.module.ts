83895616ce12a62d3adb340180875f5bbf6a6bbc
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-12-28 10:12:12 -0300

    ok

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    PassportModule.register({
+      defaultStrategy: 'jwt',
+      property: 'user',
+      session: false,
+    }),
+    JwtModule.register({
+      secret: process.env.SECRETKEY,
+      signOptions: {
+        expiresIn: '300s',
+      },
+    }),
+  ],
+  providers: [UsuariosService, PrismaService, AuthService, JwtStrategy],
+  controllers: [AuthController],
+  exports: [PassportModule, JwtModule, AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsuariosService } from 'src/usuarios/usuarios.service';
+import { LoginDto } from './dto/login.dto';
+import { JwtPayload } from './jwt.strategy';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usuarioService: UsuariosService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async login(loginUserDto: LoginDto) {
+    const user = await this.usuarioService.findByLogin(loginUserDto);
+
+    const token = this._createToken(user);
+
+    return {
+      email: user.email,
+      ...token,
+    };
+  }
+
+  private _createToken({ email }: LoginDto): any {
+    const user: JwtPayload = { email };
+    const accessToken = this.jwtService.sign(user);
+    return {
+      expiresIn: process.env.EXPIRESIN,
+      accessToken,
+    };
+  }
+}
